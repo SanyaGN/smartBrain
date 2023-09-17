a658c17ae932b31aa0fd0be3919207331a7db658
sanyagrig <sanyagrigoryan1985@gmail.com>
2023-09-17 22:50:41 +0400

    improved test data class

--- src/test/java/com/smartbrain/configs/TestingDataForUnitTests.java
+++ src/test/java/com/smartbrain/configs/TestingDataForUnitTests.java
@@ -10,43 +10,37 @@ public class TestingDataForUnitTests {
 
     private static final String DATE_FORMAT = "dd/MM/yyyy";
 
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+
     public String eighteen() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         return LocalDate.now().minusYears(18).format(formatter);
     }
 
     public String eighteenAbove() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         return LocalDate.now().minusYears(28).format(formatter);
     }
 
     public String lessThanEighteen() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         return LocalDate.now().minusYears(8).format(formatter);
     }
 
     public String eighteenPlusOneDay() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         return LocalDate.now().minusYears(18).minusDays(1).format(formatter);
     }
 
     public String eighteenMinusOneDay() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         return LocalDate.now().minusYears(18).plusDays(1).format(formatter);
     }
 
     public String hundred() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         return LocalDate.now().minusYears(100).format(formatter);
     }
 
     public String hundredMinusOneDay() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         return LocalDate.now().minusYears(100).plusDays(1).format(formatter);
     }
 
     public String zero() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
         return LocalDate.now().format(formatter);
     }
 }
